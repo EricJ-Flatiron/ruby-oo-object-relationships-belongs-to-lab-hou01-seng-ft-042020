a736e1b1403331aab2a1ed6bdea0fa4caaae8ffe
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-23 15:50:37 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+    require 'artist.rb'
+    attr_accessor :title , :artist
+
+    # def initialize(title , song)
+    #     @title = title 
+    #     @song = song
+    # end
+
+
+
+
+end
